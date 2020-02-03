2388e2b135d1cef3f68415c1ab3d4287e5ca543f
meilinglin2017 <43161778+meilinglin2017@users.noreply.github.com>
2020-02-03 08:41:06 +0800

    hello

---
+++ SMT203 Assignment 1.py
@@ -0,0 +1,23 @@
+import requests 
+import json
+
+# add other import statements here if nec
+
+##############################################################
+# global variables 
+##############################################################
+
+chat_id = None # fill in your chat id here
+api_token = '' # fill in your api token here 
+
+# add other global variables here 
+
+##############################################################
+# mood_tracker 
+##############################################################
+
+def mood_tracker(chat_id, interval_sec):
+
+	# write your code here
+
+	return
