3c5beb678722a5db2a1ac332768375c1f2b153c1
meilinglin2017 <43161778+meilinglin2017@users.noreply.github.com>
2020-02-05 16:23:04 +0800

    step

--- SMT203_Assignment_1.py
+++ SMT203_Assignment_1.py
@@ -19,16 +19,18 @@ sendPhoto_url = base_url + 'sendPhoto'
 getUpdates_url = base_url + 'getUpdates'
 
 # add other global variables here 
-# def send_msg(chat_id,msg_text):
-# 	my_url = "https://api.telegram.org/bot" + api_token + "/sendMessage"
-# 	my_params = {"chat_id": chat_id, "text":msg_text}
-# 	r = requests.get(my_url, params=my_params)
-# 	if r.status_code == 200:
-# 		return r.json()['result']['message_id']
-# 	return r.status_code
+def send_msg(chat_id,msg_text):
+	my_url = "https://api.telegram.org/bot" + api_token + "/sendMessage"
+	my_params = {"chat_id": chat_id, "text":msg_text}
+	r = requests.get(my_url, params=my_params)
+	if r.status_code == 200:
+		return r.json()['result']['message_id']
+	return r.status_code
 
-# def retrieve_msg(chat_id):
-# 	return r.json()['result'][0]['message']['text']
+def retrieve_msg(chat_id):
+	my_url = getUpdates_url 
+	r = requests.get(my_url)
+	return r.json()['result'][0]['message']['text']
 ##############################################################
 # mood_tracker 
 ##############################################################
@@ -38,20 +40,26 @@ def mood_tracker(chat_id, interval_sec):
 	# write your code here
 
 	# get latest update id before this echobot 
-	params = {'offset': 0}
-	r = requests.get(url=getUpdates_url, params=params)
+	# params = {'offset': 0}
+	# r = requests.get(url=getUpdates_url, params=params)
 	# we use try except here, because r.json()['result'] may be an empty list
 	# alternatively, you can use IF-THEN to check that list is not empty 
-	try:
-		previous_id = r.json()['result'][-1]['update_id']
-	except:
-		previous_id = 0 
-	print(r.status_code)
-	print('previous_id', previous_id)
+	msg_text = "Please rate your current mood: 1(poor) to 5(excellent)"
+	send_msg(chat_id,msg_text)
 
-	while True:
-		number = retrieve_msg(chat_id) 
-		if number in range (1,6)
+	# text = retrieve_msg(chat_id)
+
+	# sleep(interval_sec)
+	# try:
+	# 	previous_id = r.json()['result'][-1]['update_id']
+	# except:
+	# 	previous_id = 0 
+	# print(r.status_code)
+	# print('previous_id', previous_id)
+
+	# while True:
+	# 	number = retrieve_msg(chat_id) 
+	# 	if number in range (1,6)
 
 	# # Time Tracker? To check every interval, while statement
 	# # If-else statement defining enter number range and the message that you want to send
@@ -61,6 +69,6 @@ def mood_tracker(chat_id, interval_sec):
 	# # GET AVERAGE OF THE LAST 10 Datapoints that was keyed in by the user
 
 	# 	sleep(3600)
+	return text
 
-
-	return
+mood_tracker(chat_id, 2)
