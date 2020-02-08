04fe86af330f84616f2eb89aa9127b7e8e444c8d
meilinglin2017 <43161778+meilinglin2017@users.noreply.github.com>
2020-02-09 01:31:17 +0800

    kanye is da best

--- SMT203_Assignment_1.py
+++ SMT203_Assignment_1.py
@@ -10,21 +10,23 @@ from datetime import datetime,timedelta
 ##############################################################
 # Change your chat id here
 chat_id = 385623042
-api_token = '1026444413:AAGgoFtrF4hAdQw3cWhKRKhtCPpr_bLxajU' # fill in your api token here 
+
+# fill in your api token here 
+api_token = '1026444413:AAGgoFtrF4hAdQw3cWhKRKhtCPpr_bLxajU' 
 
 base_url = 'https://api.telegram.org/bot{}/'.format(api_token)
 sendMsg_url = base_url + 'sendMessage'
-editMsg_url = base_url + 'editMessageText'
 sendPhoto_url = base_url + 'sendPhoto'
 
 getUpdates_url = base_url + 'getUpdates'
 
-cat_url = 'https://cataas.com/cat/cute/says/'
+quotes_url = 'https://api.kanye.rest/'
+cat_url = 'https://aws.random.cat/meow?ref=apilist.fun'
 
 # add other global variables here 
 # Trigger to send message
 def send_msg(chat_id,msg_text):
-	my_params = {"chat_id": chat_id, "text":msg_text}
+	my_params = {"chat_id": chat_id, "text":msg_text, 'parse_mode':'Markdown'}
 	r = requests.get(url=sendMsg_url, params=my_params)
 	if r.status_code == 200:
 		return r.json()['result']['message_id']
@@ -43,7 +45,7 @@ def get_latest_text(offset):
 	my_params = {"offset": offset}
 	r = requests.get(url=getUpdates_url,params=my_params)
 	text = r.json()['result'][0]['message']['text']
-	print(text)
+	# print(text)
 	return text 
 
 # Check if the input is a float/number then if it is a string ask user to enter a number
@@ -51,38 +53,44 @@ def process_input(input):
 	try: 
 		number = float(input)
 		if number > 5 or number < 0:
-			output = 'Please key in a **NUMBER** from 1 to 5'
+			output = 'Please key in a *NUMBER* from *1* to *5*'
 			verification = 0
 		else:
 			if 0 <= number <= 3:
-				output = 'Oh dear, hope you are feeling better soon!'
+				output = '_Oh dear_, *hope you are feeling better soon!*'
 			elif 3 <= number <= 4:
-				output = 'Meowwww WOW NUBBAD!'
+				output = '_Meowwww_ *WOW NUBBAD!*'
 			else:	
-				output = 'RAWRRRRR! Keep it up!'
+				output = '_RAWRRRRR_! *Keep it up!*'
 			verification = 1
 	except:
-		output = 'Please key in a **NUMBER** from 1 to 5'
+		output = 'Please key in a *NUMBER* from *1* to *5*'
 		verification = 0
 	return [output,verification]
 
-# Report average and number of datapoints that was given
 def avg_datapoints(num_list):
 	if len(num_list) > 10:
 		num_list.pop(0)
-	print(num_list)
+	# print(num_list)
 	len_list = len(num_list)
 	average = round(sum(num_list)/len(num_list),2)
 	return [str(len_list),str(average)]
 
-def send_image(chat_id,text):
-	s = requests.session()
-	photo_url = cat_url + text
-	my_headers= {'Cache-Control': 'no-cache'}
+def retreive_quote():
+	r = requests.get(url=quotes_url)
+	text = 'As the famous *Kanye West* says: _' + r.json()['quote'] + '_'
+	return text
+
+def retrieve_imgurl():
+	r = requests.get(url=cat_url)
+	img_url = r.json()['file']
+	return img_url
+
+def send_image(chat_id,photo_url):
+	photo_url = retrieve_imgurl()
 	my_params = {'chat_id': chat_id, 'photo':photo_url}
-	r = s.get(url=sendPhoto_url,params=my_params, headers=my_headers)
-	print(json.dumps(r.json(),indent=2,sort_keys=True))
-	s.cookies.clear()
+	r = requests.get(url=sendPhoto_url,params=my_params)
+	# print(json.dumps(r.json(),indent=2,sort_keys=True))
 	if r.status_code == 200:
 		return r.json()['result']['message_id']
 	return r.status_code
@@ -114,12 +122,8 @@ def mood_tracker(chat_id, interval_sec):
 				count_response = 0
 				reminder = 0
 
-			# print(get_latest_offset())
-			# print("now " + str(datetime.now()))
-			# print("future " + str(future_time))
-			
 			if datetime.now() >= halftime_mark and reminder == 0 and count_response == 0:
-				output_response = "Please remember to send in your mood by " + str(future_time) + " !"
+				output_response = "Please remember to send in your mood by _" + str(future_time)[:19] + "_ !"
 				send_msg(chat_id,output_response)
 				reminder += 1
 
@@ -127,21 +131,20 @@ def mood_tracker(chat_id, interval_sec):
 				current_offset = get_latest_offset()
 				user_response = get_latest_text(current_offset)
 				output_response = process_input(user_response)
-				# print("response count: " + str(count_response))
 
 				if count_response >= 1:
-					output_response = "You have already sent in your mood, please try again at " + str(future_time)
+					output_response = "You have already sent in your mood, please try again at _" + str(future_time)[:19] + "_"
 					send_msg(chat_id,output_response)
 
 				elif count_response == 0:
 					if output_response[1] == 1:
 						count_response += 1
 						num_list.append(float(user_response))
-						output_datapoints = " Your avg mood for the last " + avg_datapoints(num_list)[0] + " data points is " + avg_datapoints(num_list)[1]
-						send_image(chat_id,output_response[0])
-						print('image sent')
+						output_datapoints = " Your avg mood for the last *" + avg_datapoints(num_list)[0] + "* data points is _" + avg_datapoints(num_list)[1] + "_"
+						send_msg(chat_id,output_response[0])
+						send_image(chat_id,retrieve_imgurl())
+						send_msg(chat_id,retreive_quote())
 						send_msg(chat_id,output_datapoints)
-						print('message sent')
 					else:
 						send_msg(chat_id,output_response[0])
 
@@ -152,4 +155,4 @@ def mood_tracker(chat_id, interval_sec):
 			exit()
 	return 
 
-mood_tracker(chat_id,10)
+mood_tracker(chat_id,20)
